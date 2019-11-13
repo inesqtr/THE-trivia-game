26651a5156f9f155c2aa0e9b05f47062cdd9080d
ines <inesqtr@gmail.com>
2019-11-13 10:20:36 +0000

    created component results

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import Home from './Home.js'
+import Home from './Home'
 import Questions from './Questions'
+import Results from './Results'
 
 
 function App() {
   return (
     <div className="App">
-        <title>THE Trivia Game</title>
+      <title>THE Trivia Game</title>
       <h1>THE Trivia Game</h1>
       <Switch>
-        <Route exact path="/home"/>
-        <Route exact path="/" component={Questions}/>
-        <Route exact path="/result"/>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/questions" component={Questions} />
+        <Route exact path="/result" component={Results} />
       </Switch>
     </div>
   );
---
+++ src/Components/Results.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Results = () => {
+    return (
+        <div>
+            <div>Your result was:</div>
+            <div>Result</div>
+            <button>PLAY AGAIN</button>
+        </div>
+    )
+}
+
+
+
+export default Results;
