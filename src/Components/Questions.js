91efc68ff84ec6b79d856c0c643236fe0e64500a
Carolina Viero <carolinaviero1@gmail.com>
2019-11-13 10:10:26 +0000

    questions page structure

---
+++ src/Components/Questions.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Questions = () => {
+    return (
+        <div>
+            <h3>Question will be displayed here</h3>
+            <div>
+                <p>Answer one</p>
+            </div>
+            <div>
+                <p>Answer two</p>
+            </div>
+            <div>
+                <p>Answer three</p>
+            </div>
+            <div>
+                <p>Answer four</p>
+            </div>
+            <div>Question number here</div>
+            <div>Timer here</div>
+            <div>Home icon here</div>
+        </div>
+    )
+}
+
+export default Questions;
