27a99c7cb9957f833beafb64af82d8d4aa8276e1
Carolina Viero <carolinaviero1@gmail.com>
2019-11-13 16:34:14 +0000

    merge

--- src/Components/EachQuestion.js
+++ src/Components/EachQuestion.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Answer from './Answer'
 
-const EachQuestion = ({ questions }) => {
+const EachQuestion = ({ question }) => {
     return (
         <div>
-            <h3>{questions}</h3>
+            <h3>{question.question}</h3>
             <Answer />
             <div>Question number here</div>
             <button>Next</button>
