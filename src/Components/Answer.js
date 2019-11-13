1d1cd0655a941df5d840f3c52af5088841603bae
ines <inesqtr@gmail.com>
2019-11-13 11:19:42 +0000

    added answer component

---
+++ src/Components/Answer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Answer = () => {
+    return (
+        <div>
+            <div>Answer 1</div>
+            <div>Answer 2</div>
+            <div>Answer 3</div>
+            <div>Answer 4</div>
+        </div>
+    )
+}
+
+export default Answer;
